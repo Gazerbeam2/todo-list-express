bd8196583912ec5583d82b5afdef5e6d400f8b4a
Antonius <jordan_smith47@yahoo.com>
2024-05-19 19:42:10 -0500

    added comments

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express') //This variable that allows us to use the express module
 const app = express() //This is a variable that allows us to use express as a method. 
 const MongoClient = require('mongodb').MongoClient //This variable set ups MongoDB allows us to use MongoDB in the code 
-const PORT = 2121 //This is the port will be using for this app
-require('dotenv').config()
+const PORT = 2121 //This is the port will be using for this app. 
+require('dotenv').config() //Allows us to use enviroment variables from the DotEnv middleware. 
 
-
+s
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbConnectionStr = process.env.DB_STRING, //This allows us to use environmental variables as a variable 
+    dbName = 'todo' //This variable sets the Database Name.
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //This code connects to MongoDB database
     .then(client => {
         console.log(`Connected to ${dbName} Database`) //This sends a message to the console that it's connected to the Database
-        db = client.db(dbName)
+        db = client.db(dbName) //This creates a variable that connects the db variable to the database. 
     })
 
 app.set('view engine', 'ejs') //
 app.use(express.static('public')) //
-app.use(express.urlencoded({ extended: true })) //
-app.use(express.json()) //
+app.use(express.urlencoded({ extended: true })) //This is a middleware method that will let us recognize the incoming object as Strings and Arrays
+app.use(express.json()) //This is a middleware method that will recognize the incoming object as a json.
+
+//You can also use BodyParser
 
 
 app.get('/',async (request, response)=>{ //The app will send a get request from the '/'(requested endpoint) and waits for a response.
-    const todoItems = await db.collection('todos').find().toArray() 
+    const todoItems = await db.collection('todos').find().toArray() //This finds the items in the database, converts them into an array and then stores it in a variable.
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
@@ -35,11 +37,11 @@ app.get('/',async (request, response)=>{ //The app will send a get request from 
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => { //This method sends a post request to the server, /addTodo is the path 
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+app.post('/addTodo', (request, response) => { //This method sends a post request to the server, /addTodo is the path we're sending the POST request to.
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) //insertOne adds items to a collection
     .then(result => {
-        console.log('Todo Added') //
-        response.redirect('/') //This refereshes the page so it can update. 
+        console.log('Todo Added') //Logs to the console that the Todo list was updated.
+        response.redirect('/') //This refreshes the page in order to show the update.
     })
     .catch(error => console.error(error)) //This code catches any error and reports it to the console. 
 })
@@ -50,7 +52,7 @@ app.put('/markComplete', (request, response) => {
             completed: true
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, //Sorts documents by their ids in descending order.
         upsert: false
     })
     .then(result => {
@@ -67,7 +69,7 @@ app.put('/markUnComplete', (request, response) => {
             completed: false
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, //This sends a message to the console that the collection has been updated. 
         upsert: false
     })
     .then(result => {
